8bf09c94d76be40023bda045dc74a21cf999d6f2
Rishu Kumar <65711673+imrishuroy@users.noreply.github.com>
2026-07-03 22:33:02 +0530

    Add: palindromic-substrings - submission-0

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-0.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int countSubstrings(String s) {
+        int n = s.length();
+        int count = 0;
+        int[][] memo = new int[n][n];
+
+        for (int left = 0; left < n; left++) {
+            for (int right = left; right < n; right++) {
+                boolean check = isPalinDrome(left, right, s, memo);
+                if (check) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    private boolean isPalinDrome(int left, int right, String s, int[][] memo) {
+        if (left >= right) {
+            return true;
+        }
+
+        if (s.charAt(left) != s.charAt(right)) {
+            memo[left][right] = 0;
+            return false; 
+        }
+
+        boolean check = isPalinDrome(left + 1, right - 1, s, memo);
+        memo[left][right] = check ? 1 : 0;
+
+        return check;
+    }
+}
