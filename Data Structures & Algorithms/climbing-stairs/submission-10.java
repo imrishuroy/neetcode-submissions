56b7b510984cdbc3d169a63267c2af211c09ccea
Rishu Kumar <65711673+imrishuroy@users.noreply.github.com>
2026-07-05 12:39:06 +0530

    Add: climbing-stairs - submission-10

---
+++ Data Structures & Algorithms/climbing-stairs/submission-10.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+
+        // memoization
+        // int[] memo = new int[n + 1];
+        // Arrays.fill(memo, -1);
+        // return climbStairsHelper(n, memo);
+    }
+
+    private int climbStairsHelper(int n, int[] memo) {
+        if (n <= 1) {
+            return 1;
+        }
+
+        if (memo[n] != -1) {
+            return memo[n];
+        }
+
+        int oneStep = climbStairsHelper(n - 1, memo);
+        int twoStep = climbStairsHelper(n - 2, memo);
+
+        return memo[n] = oneStep + twoStep;
+    }
+}
