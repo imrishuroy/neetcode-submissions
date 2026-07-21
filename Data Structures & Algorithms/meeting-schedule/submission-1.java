abcb092d054d9933772e482323515f55e81d6e78
Rishu Kumar <65711673+imrishuroy@users.noreply.github.com>
2026-07-21 23:01:16 +0530

    Add: meeting-schedule - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule/submission-1.java
@@ -0,0 +1,28 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean canAttendMeetings(List<Interval> intervals) {
+
+        Collections.sort(intervals, (a, b) -> Integer.compare(a.start, b.start)); // sort by start time
+
+        for (int i = 1; i < intervals.size(); i++) {
+            Interval prev = intervals.get(i - 1);
+            Interval curr = intervals.get(i);
+
+            if (curr.start < prev.end) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
